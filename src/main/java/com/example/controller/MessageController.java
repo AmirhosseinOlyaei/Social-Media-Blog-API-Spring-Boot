ecf3c3c739f9a90466b90bf1c846c2307eca1063
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-09-26 03:41:16 -0400

    Get Message by Message Id

--- src/main/java/com/example/controller/MessageController.java
+++ src/main/java/com/example/controller/MessageController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,5 +46,14 @@ public class MessageController {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.ok(messages);
+    }
+
+    @GetMapping("/messages/{messageId}")
+    public ResponseEntity<Message> getMessageById(@PathVariable Integer messageId) {
+        Message message = messageService.getMessageById(messageId);
+        if (message == null) {
+            return ResponseEntity.ok().build(); // Return 200 OK with an empty body.
+        }
+        return ResponseEntity.ok(message);
     }
 }
